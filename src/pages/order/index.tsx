e00fa16d909e18f258723ca36826cf21282c6b3d
rookie <1764600907@qq.com>
2025-05-13 23:09:09 +0800

    feat: 设置 路由权限验证方式

---
+++ src/pages/order/index.tsx
@@ -0,0 +1,9 @@
+const orderList: React.FC = () => {
+    return (
+        <>
+            <h1>order list page</h1>
+        </>
+    )
+}
+
+export default orderList
--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,10 +1,30 @@
-import { createBrowserRouter } from "react-router";
+import { createBrowserRouter, redirect } from "react-router";
 import App from "../App.tsx";
+import orderList from "@/pages/order/index.tsx";
+import { getToken } from "@/utils/storage.ts";
+import Login from "@/pages/login/index.tsx";
+
+export function authLoader() {
+  const token = getToken()
+  if (!token) {
+    return redirect('/login');
+  }
+  return null;
+}
 
 const router = createBrowserRouter([
   {
     path: "/",
     Component: App,
+  },
+  {
+    path: '/login',
+    Component: Login
+  },
+  {
+    path: "/orderList",
+    loader: authLoader,
+    Component: orderList,
   },
 ]);
 
