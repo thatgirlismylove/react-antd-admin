c4bd945704bfdffea0ef6405f33d7555559c2174
rookie <1764600907@qq.com>
2025-05-14 16:42:07 +0800

    feat: 优化 menu 菜单组件，自动匹配当前页面路径，动态计算 openKeys， selectedKeys

--- src/layout/side-bar/index.tsx
+++ src/layout/side-bar/index.tsx
@@ -1,50 +1,59 @@
-import {Layout, Menu, type MenuProps} from "antd";
-import {UploadOutlined, UserOutlined, VideoCameraOutlined} from "@ant-design/icons";
-import React, {useState} from "react";
-import {appCollapsed} from "@/store";
-import {useAtom} from 'jotai';
-import {useNavigate} from "react-router";
+import { Layout, Menu, type MenuProps } from "antd";
+import {
+  UploadOutlined,
+  UserOutlined,
+  VideoCameraOutlined,
+} from "@ant-design/icons";
+import React, { useEffect, useState } from "react";
+import { appCollapsed } from "@/store";
+import { useAtom } from "jotai";
+import { useLocation, useMatches, useNavigate } from "react-router";
 
-const {Sider} = Layout;
+const { Sider } = Layout;
 
 const SideBar: React.FC = () => {
+  const { pathname  } = useLocation();
+  const matches = useMatches()
+  const navigate = useNavigate();
 
-    const navigate = useNavigate()
+  const openKeys = matches.map(match => match.pathname);  
 
-    const [collapsed] = useAtom(appCollapsed);
-    const [current, setCurrent] = useState('mail');
-    const handleClick: MenuProps['onClick'] = (e) => {
-        setCurrent(e.key)
-        navigate(e.key)
-    }
+  const [collapsed] = useAtom(appCollapsed);
 
-    return (
-        <Sider trigger={null} collapsible collapsed={collapsed}>
-            <div className="demo-logo-vertical"/>
-            <Menu
-                mode="inline"
-                selectedKeys={[current]}
-                onClick={handleClick}
-                items={[
-                    {
-                        key: '/',
-                        icon: <UserOutlined/>,
-                        label: 'Home',
-                    },
-                    {
-                        key: '/antd',
-                        icon: <VideoCameraOutlined/>,
-                        label: 'Antd Components',
-                    },
-                    {
-                        key: '/orderList',
-                        icon: <UploadOutlined/>,
-                        label: 'Order List',
-                    },
-                ]}
-            />
-        </Sider>
-    )
-}
+  const items = [
+    {
+      key: "/",
+      icon: <UserOutlined />,
+      label: "Home",
+    },
+    {
+      key: "/antd",
+      icon: <VideoCameraOutlined />,
+      label: "Antd Components",
+    },
+    {
+      key: "/orderList",
+      icon: <UploadOutlined />,
+      label: "Order List",
+    },
+  ];
 
-export default SideBar
+  const handleClick: MenuProps["onClick"] = (e) => {
+    navigate(e.key);
+  };
+
+  return (
+    <Sider trigger={null} collapsible collapsed={collapsed}>
+      <div className="demo-logo-vertical" />
+      <Menu
+        mode="inline"
+        selectedKeys={[pathname]}   
+        openKeys={openKeys}
+        onClick={handleClick}
+        items={items}
+      />
+    </Sider>
+  );
+};
+
+export default SideBar;
